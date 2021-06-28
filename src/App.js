63be75422c2511cad527d431b3385e29ba004453
Kyle Eriss <kyle.eriss@gmail.com>
2021-06-28 01:11:57 -0400

    realtime vehicle position updates rendered

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
-import React, { useState } from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React, { useEffect, useState } from 'react'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 import config from './config';
 import './App.css';
 
@@ -15,36 +15,76 @@ const center = {
   lng: -71.058571
 };
 
+const mbtaKey = config.MBTA_API_KEY
+
 function App() {
 
   const [markers, setMarkers] = useState([]);
+  const [selected, setSelected] = useState(null);
 
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: config.API_KEY
   })
 
+  useEffect(() => {
+    setInterval(() => {
+      fetch(`https://api-v3.mbta.com/vehicles?${mbtaKey}`)
+        .then(res => res.json())
+        .then((info) => {
+          setMarkers(info.data)
+        })
+    }, 1000);
+
+    console.log(mbtaKey)
+  }, [])
+
   return isLoaded ? (
+
     <div className='map'>
+
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
         zoom={13}
-        onClick={(event) => {
-          setMarkers((current) => [
-            ...current,
-            {
-              lat: event.latLng.lat(),
-              lng:event.latLng.lng()
-            },
-          ]);
-        }}
       >
+        {markers.map((marker, idx) => (
+          <Marker
+            key={idx}
+            position={
+              {
+                lat: marker.attributes.latitude,
+                lng: marker.attributes.longitude
+              }
+            }
+            onClick={() => {
+              setSelected(marker);
+            }}
+          />
+        ))}
         <></>
       </GoogleMap>
+
     </div >
 
   ) : <></>
 }
 
 export default App;
+
+
+
+
+
+
+
+
+// onClick={(event) => {
+        //   setMarkers((current) => [
+        //     ...current,
+        //     {
+        //       lat: event.latLng.lat(),
+        //       lng:event.latLng.lng()
+        //     },
+        //   ]);
+        // }}
--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
     API_KEY: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    MBTA_API_KEY: process.env.REACT_APP_MBTA_API_KEY
 };
